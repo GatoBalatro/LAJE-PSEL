e2ddb274858650b3be1820a9d57e8f0bb08968d5
Vinicius Brandao <vinicius.brandaot@gmail.com>
2026-05-23 12:38:36 -0300

    feat/page_compiler

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSingleFile({
+      // Opcional: remove o loader do Vite (recomendado)
+      removeViteModuleLoader: true,
+    })
+  ],
+  build: {
+    // Opcional: aumenta o limite para inlining de assets (imagens, etc)
+    assetsInlineLimit: 100000000, // ~100MB (cuidado com tamanho final)
+  }
 })
